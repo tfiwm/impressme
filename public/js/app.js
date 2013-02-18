1ad94762e6c5cbefc517fe0f9f6f60b19e7c1c45
Mitko Tschimev <mitko.tschimev@icloud.com>
2013-02-18 09:41:26 +0100

    new play! structure

---
+++ public/js/app.js
@@ -0,0 +1,38 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mtschimev
+ * Date: 20.01.13
+ * Time: 17:09
+ */
+
+/**
+ * SlideContainer is a singleton instance
+ */
+define(
+    [
+        'jquery',
+        'underscore',
+        'jqueryui',
+        'backbone',
+        'ckeditor',
+        'views/slides/SlideContainer',
+        'views/slides/SlideContent'
+    ],
+
+    function ($, _, Jqueryui, Backbone, CKEditor, SlideContainer, SlideContent) {
+
+        function initialize() {
+            var slideContainer = new SlideContainer(),
+                slideContent = new SlideContent();
+
+            slideContainer.on('selectSlide', function (slideModel) {
+                slideContent.showSlide(slideModel);
+            });
+        }
+
+        return {
+            initialize: initialize
+        };
+    }
+);
+
